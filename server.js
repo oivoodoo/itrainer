a2062d8946c78a47c1748925fb9bbd63c1f87c8c
Alexander Korsak <alex.korsak@gmail.com>
2011-03-22 13:39:36 +0000

    Add submodules

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
-var sys = require("sys");
+require.paths.unshift('./modules/');
 
-console.log("create simple node server");
+var sys = require("sys")
+    ,   express = require('express')
+    ,   mongoose = require('mongoose')
+    ,   connect = require('connect')
+    ,   mongoStore = require('connect-mongodb');
+    
+var app = module.exports = express.createServer(
+    connect.bodyParser()
+    ,   connect.methodOverride()
+    );
+
+/* Include controllers of the application with the full routes */
+// require('./controllers')(app);
+
+/* Include helpers of the application */
+
+/* Run the server with default env params */
+if (!module.parent) {
+    app.listen(process.env.PORT || 3000);
+    console.log('Express server listening on port %d, environment: %s', app.address().port, app.settings.env)
+    console.log('Using Express %s, Jade %s', express.version, jade.version);
+}
