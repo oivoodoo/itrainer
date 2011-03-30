1050e8da6b7af8e04715101ddadeb1c2ebb0461c
Alexander Korsak <alex.korsak@gmail.com>
2011-03-30 08:48:20 +0000

    Updates

---
+++ helpers/index.js
@@ -0,0 +1,17 @@
+var property = require('./property')
+
+module.exports = function(app) {
+  app.dynamicHelpers({
+    messages: require('express-messages')
+    , admin_menu: function() {
+      return property.create();
+      }
+    , assets: function() {
+      return app.config.assets.handler.cacheTimestamps;
+      }
+  });
+  
+  app.helpers({
+    helper: require('./application')
+  });
+}
