638ea00f069f55a6469f6343590d082c3b97e7dc
Alexander Korsak <alex.korsak@gmail.com>
2011-03-30 23:16:01 +0300

    updates

--- server.js
+++ server.js
@@ -25,20 +25,21 @@ var app = module.exports = express.createServer(
     , connect.methodOverride()
     , connect.favicon()
     , i18n({default_locale: 'ru_ru'}, function(locals) { console.log(locals); })
-    // , connect.session({ secret: '9FF96302-4633-11E0-AAE4-38FEDED72085' })
+    , connect.session({ secret: '9FF96302-4633-11E0-AAE4-38FEDED72085' })
 );
-
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  // app.use(express.session({store: mongoStore(app.set('db-uri')), secret: '9FF96302-4633-11E0-AAE4-38FEDED72085'}))
-  app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }))
-});
 
 app.config = config;
 app.mongoose = mongoose;
 
 require('./config/db')(app);
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.session({store: mongoStore(app.set('db-uri')), secret: '9FF96302-4633-11E0-AAE4-38FEDED72085'}))
+  app.use(express.logger({ format: '\x1b[1m:method\x1b[0m \x1b[33m:url\x1b[0m :response-time ms' }))
+});
+
 require('./models')(app);
 require('./controllers')(app);
 require('./helpers')(app);
