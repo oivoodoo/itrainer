47cf245c0542e58f4d9f99b11c8fc40b688b3ab2
Alexander Korsak <alex.korsak@gmail.com>
2011-03-30 08:33:58 +0000

    Add submodules and create loader for application models

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,12 @@
-empty file.
+var fs = require('fs')
+    , path = require('path');
+
+module.exports = function(app) {
+  var files = fs.readdirSync(__dirname);
+  files.forEach(function(f) {
+    var basename = path.basename(f, '.js');
+    if (basename != 'index') {
+      require('./' + basename)(app);
+    }
+    });
+  };
