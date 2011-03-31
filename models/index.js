6f97458d6c951775c9cd0dbebe7000a87bd595d4
Alexander Korsak <alex.korsak@gmail.com>
2011-03-31 10:12:09 +0000

    Change loader for models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,11 @@ var fs = require('fs')
     , path = require('path');
 
 module.exports = function(app) {
-  
+  require('./roles')(app);
+  require('./users')(app);
+  require('./login_token')(app);
+  require('./city')(app);
+  require('./city_region')(app);
+  require('./service')(app);
+  require('./service_category')(app);
 };
